1977b2ca6b1e332344d6025241ea2712461f082d
Daksh Sharma <geekdaksh@gmail.com>
2025-08-13 17:03:48 +0100

    Improve code readability

--- src/Todo_CLI/cli.py
+++ src/Todo_CLI/cli.py
@@ -1,102 +1,22 @@
-import json
 from rich.console import Console
 import typer
 from typing_extensions import Annotated
 import questionary
 from prompt_toolkit.styles import Style
-from services.task_service import TaskService
-from storage.local import LocalStorage
-from views.user_views import render_tasks
-from settings import FILE_PATH
+
+from Todo_CLI.settings import FILE_PATH, QUESTIONARY_THEME
+from Todo_CLI.services.task_service import TaskService
+from Todo_CLI.storage.local import LocalStorage
+from Todo_CLI.views.user_views import render_tasks
 
 console = Console()
 
-
-def get_tasks(tasks):
-    with open("tasks.json", "r") as file:
-        task_json = json.load(file)
-
-    for idx in range(len(task_json["tasks"])):
-        task_name = task_json["tasks"][idx]["name"]
-        task_status = task_json["tasks"][idx]["status"]
-        task_notes = task_json["tasks"][idx]["notes"]
-
-        new_task = task.Task(task_name, task_status, task_notes)
-        tasks.add_task(new_task)
-
-    return tasks
-
-
-# def list_tasks(tasks, nerd):
-#
-#     if nerd:
-#         for idx, task in enumerate(tasks):
-#             if task.status == "Complete":
-#                 task_fmt = f"""[green]󰄳 {task.name}\n  {task.notes}[/green]"""
-#
-#                 console.print(task_fmt)
-#             elif task.status == "in progress":
-#                 task_fmt = f"""[yellow]󰄯 {task.name}\n  {task.notes}[/yellow]"""
-#
-#                 console.print(task_fmt)
-#             elif task.status == "todo":
-#                 task_fmt = f"""[bright_black]󰄰 {task.name}\n  {task.notes}[/bright_black]"""
-#
-#                 console.print(task_fmt)
-#     else:
-#         for idx, task in enumerate(tasks):
-#             if task.status == "complete":
-#                 task_fmt = f"""[green]◉ {task.name}\n  {task["notes"]}[/green]"""
-#
-#                 console.print(task_fmt)
-#             elif task.status == "in progress":
-#                 task_fmt = f"""[yellow]◐ {task.name}\n  {task.notes}[/yellow]"""
-#
-#                 console.print(task_fmt)
-#             elif task.status == "todo":
-#                 task_fmt = f"""[bright_black]◌ {task.name}\n  {task.notes}[/bright_black]"""
-#
-#                 console.print(task_fmt)
 
 def main(nerd: Annotated[bool, typer.Option(help="Use nerd font glyphs")] = False,
          interactive: Annotated[bool, typer.Option(help="Use interactive mode for CLI")] = False):
     actions = ["list", "add", "remove", "change", "quit"]
 
-    catppuccin_mocha = {
-        "rosewater": "#f5e0dc",
-        "flamingo": "#f2cdcd",
-        "pink": "#f5c2e7",
-        "mauve": "#cba6f7",
-        "red": "#f38ba8",
-        "maroon": "#eba0ac",
-        "peach": "#fab387",
-        "yellow": "#f9e2af",
-        "green": "#a6e3a1",
-        "teal": "#94e2d5",
-        "sky": "#89dceb",
-        "sapphire": "#74c7ec",
-        "blue": "#89b4fa",
-        "lavender": "#b4befe",
-        "text": "#cdd6f4",
-        "subtext1": "#bac2de",
-        "overlay2": "#9399b2",
-        "surface2": "#585b70",
-        "base": "#1e1e2e",
-    }
-
-    main_style = Style.from_dict({
-        'qmark': f"fg:{catppuccin_mocha['mauve']} bold",
-        'question': f"fg:{catppuccin_mocha['text']} bold",
-        'answer': f"fg:{catppuccin_mocha['green']} bold",
-        'pointer': f"fg:{catppuccin_mocha['blue']} bold",
-        'highlighted': f"fg:{catppuccin_mocha['pink']} bold",
-        'selected': f"fg:{catppuccin_mocha['peach']} bold",
-        'separator': f"fg:{catppuccin_mocha['surface2']}",
-        'instruction': f"fg:{catppuccin_mocha['overlay2']} italic",
-        'text': f"fg:{catppuccin_mocha['text']}",
-        'disabled': f"fg:{catppuccin_mocha['overlay2']} italic",
-    })
-
+    main_style = Style.from_dict(QUESTIONARY_THEME)
     while True:
         if interactive:
             action = questionary.rawselect(
@@ -162,6 +82,7 @@ def main(nerd: Annotated[bool, typer.Option(help="Use nerd font glyphs")] = Fals
 
             case "quit":
                 exit("Bye!")
+
             case _:
                 console.print("[red]An unknown error occurred.[/red]")
                 continue
--- src/Todo_CLI/settings.py
+++ src/Todo_CLI/settings.py
@@ -2,3 +2,38 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 FILE_PATH = os.path.join(ROOT_DIR, 'tasks.json')
+
+CATPPUCCIN_MOCHA = {
+    "rosewater": "#f5e0dc",
+    "flamingo": "#f2cdcd",
+    "pink": "#f5c2e7",
+    "mauve": "#cba6f7",
+    "red": "#f38ba8",
+    "maroon": "#eba0ac",
+    "peach": "#fab387",
+    "yellow": "#f9e2af",
+    "green": "#a6e3a1",
+    "teal": "#94e2d5",
+    "sky": "#89dceb",
+    "sapphire": "#74c7ec",
+    "blue": "#89b4fa",
+    "lavender": "#b4befe",
+    "text": "#cdd6f4",
+    "subtext1": "#bac2de",
+    "overlay2": "#9399b2",
+    "surface2": "#585b70",
+    "base": "#1e1e2e",
+}
+
+QUESTIONARY_THEME = {
+        'qmark': f"fg:{CATPPUCCIN_MOCHA['mauve']} bold",
+        'question': f"fg:{CATPPUCCIN_MOCHA['text']} bold",
+        'answer': f"fg:{CATPPUCCIN_MOCHA['green']} bold",
+        'pointer': f"fg:{CATPPUCCIN_MOCHA['blue']} bold",
+        'highlighted': f"fg:{CATPPUCCIN_MOCHA['pink']} bold",
+        'selected': f"fg:{CATPPUCCIN_MOCHA['peach']} bold",
+        'separator': f"fg:{CATPPUCCIN_MOCHA['surface2']}",
+        'instruction': f"fg:{CATPPUCCIN_MOCHA['overlay2']} italic",
+        'text': f"fg:{CATPPUCCIN_MOCHA['text']}",
+        'disabled': f"fg:{CATPPUCCIN_MOCHA['overlay2']} italic",
+    }
