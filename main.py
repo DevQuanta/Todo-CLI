04a78f47d9d12ab690abc4eb10427faca05f54d7
Daksh Sharma <geekdaksh@gmail.com>
2025-08-13 10:51:31 +0100

    Pretty output with optional support for nerd font glyphs

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import json
+from rich.console import Console
+import typer
+from typing_extensions import Annotated
 
+
+
+
+console = Console()
 
 def get_tasks():
     with open("tasks.json", "r") as file:
@@ -37,17 +44,46 @@ def remove_task(tasks, idx):
 
     commit(tasks)
 
+def list_tasks(tasks, nerd):
+    task_list = tasks["tasks"]
 
-def main():
+    if nerd:
+        for idx, task in enumerate(task_list):
+            if task["status"] == "Complete":
+                task_fmt = f"""[green]󰄳 {task["name"]}\n  {task["notes"]}[/green]"""
+
+                console.print(task_fmt)
+            elif task["status"] == "In Progress":
+                task_fmt = f"""[yellow]󰄯 {task["name"]}\n  {task["notes"]}[/yellow]"""
+
+                console.print(task_fmt)
+            elif task["status"] == "Todo":
+                task_fmt = f"""[bright_black]󰄰 {task["name"]}\n  {task["notes"]}[/bright_black]"""
+
+                console.print(task_fmt)
+    else:
+        for idx, task in enumerate(task_list):
+            if task["status"] == "Complete":
+                task_fmt = f"""[green]◉ {task["name"]}\n  {task["notes"]}[/green]"""
+
+                console.print(task_fmt)
+            elif task["status"] == "In Progress":
+                task_fmt = f"""[yellow]◐ {task["name"]}\n  {task["notes"]}[/yellow]"""
+
+                console.print(task_fmt)
+            elif task["status"] == "Todo":
+                task_fmt = f"""[bright_black]◌ {task["name"]}\n  {task["notes"]}[/bright_black]"""
+
+                console.print(task_fmt)
+
+def main(nerd: Annotated[bool, typer.Option(help="Use Nerd Font glyphs")] = False,):
     while True:
         action = input("add, list, change, or remove tasks: ")
         tasks = get_tasks()
 
         match action.lower():
             case "list":
-                for idx, task in enumerate(tasks["tasks"]):
-                    print(f"---TASK {idx + 1}---")
-                    print(f"Name: {task["name"]}\nStatus: {task["status"]}\nNotes: {task["notes"]}")
+                list_tasks(tasks, nerd)
 
             case "add":
                 task = input("Task name: ")
@@ -76,4 +112,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
